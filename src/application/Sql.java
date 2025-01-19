2bec5da78aa0a254b8a03144d65f2ed0f3a9ce18
Mohamed-ds-au <111690029+Mohamed-ds-au@users.noreply.github.com>
2025-01-19 16:50:08 +0200

    Update Sql.java

--- src/application/Sql.java
+++ src/application/Sql.java
@@ -6,7 +6,7 @@ public class Sql {
 	public static void Connect() throws SQLException, ClassNotFoundException {
 		//Class.forName( "com.mysql.jdbc.Driver");
 		String sql = "select * from books";
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","mysql-passord");  
 		Statement st=con.createStatement();
 		ResultSet rs = st.executeQuery(sql);
 		
@@ -16,7 +16,7 @@ public class Sql {
 	
 	public static void searchBook(String name) throws SQLException {
 		String query = "select * from books where title = \""+name+"\"";
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","mysql-password");  
 		Statement st=con.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		rs.next();
@@ -32,7 +32,7 @@ public class Sql {
 	
 	public static void addBook(int isbn, String author, String genere, int year, String title,int availble ) throws SQLException {
 		String query  = "insert into books values (?,?,?,?,?,?)";
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 		PreparedStatement st=con.prepareStatement(query);
 		st.setInt(1, isbn);
 		st.setString(2, author);
@@ -46,14 +46,14 @@ public class Sql {
 	
 	public static void deleteBook(int isbn) throws SQLException {
 		String query = "delete from books where isbn = "+isbn;
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 		Statement st=con.createStatement();
 		int count = st.executeUpdate(query);
 		System.out.println(count+" row/s is effected");
 	}
 	public static void showTables() throws SQLException {
 		String query = "show tables";
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 		Statement st=con.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		while(rs.next()) {
@@ -63,7 +63,7 @@ public class Sql {
 	
 	public static void showBookForMem() throws SQLException {
 		String query = "select * from books";
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 		Statement st=con.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		while(rs.next()) {
@@ -73,7 +73,7 @@ public class Sql {
 	 public static void showEmployees() throws SQLException {
 		 
 		 String query = "select * from employee";
-			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 			Statement st=con.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while(rs.next()) {
@@ -83,7 +83,7 @@ public class Sql {
 	 public static void showBorrowedBooks() throws SQLException {
 		 
 		 String query = "select * from borrowed_books";
-		 Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+		 Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 		 Statement st=con.createStatement();
 		 ResultSet rs = st.executeQuery(query);
 		 while(rs.next()) {
@@ -95,14 +95,14 @@ public class Sql {
 	 }
 	 public static void deleteMember(int id) throws SQLException {
 			String query = "delete from members where id = "+id;
-			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 			Statement st=con.createStatement();
 			int count = st.executeUpdate(query);
 			System.out.println(count+" row/s is effected");
 		}
 	 public static void addMember( String pass, String email, String member_name, int zip_code,String state, String street, String city ) throws SQLException {
 			String query  = "insert into members(mem_pass, mem_email, member_name,zip_code,state, street, city) values (?,?,?,?,?,?,?)";
-			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","1234");  
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");  
 			PreparedStatement st=con.prepareStatement(query);
 			
 			st.setString(1, pass);
